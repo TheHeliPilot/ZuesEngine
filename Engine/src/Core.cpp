95e0b8ef32dacb2b04a20341a83ad522457adf3a
TheHeliPilot <bucka.samuel@gmail.com>
2025-09-26 20:54:08 +0200

    Init

---
+++ Engine/src/Core.cpp
@@ -0,0 +1,15 @@
+﻿//
+// Created by bucka on 9/26/2025.
+//
+
+#include "../include/Engine/Core.h"
+
+void Engine::Core::Init() {
+
+}
+
+void Engine::Core::Update() {
+}
+
+void Engine::Core::Shutdown() {
+}
