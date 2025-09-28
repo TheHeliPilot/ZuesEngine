4bca597a0567e481f98b528be8db5680469cdebc
TheHeliPilot <bucka.samuel@gmail.com>
2025-09-28 21:32:26 +0200

    Comment out debug logging in ViewportCameraSystem
    The ENGINE_LOG statement logging deltaTime in ViewportCameraSystem::Update has been commented out, likely to reduce console output during normal operation.

--- Engine/include/Engine/ECS/Systems/ViewportCameraSystem.h
+++ Engine/include/Engine/ECS/Systems/ViewportCameraSystem.h
@@ -12,7 +12,7 @@ class ViewportCameraSystem final : public SystemBase<PositionComponent*, CameraC
 public:
     void Update(const float deltaTime, PositionComponent* pos, CameraComponent* cam, ViewportCameraTag* vc_tag) override {
 
-        ENGINE_LOG(std::to_string(deltaTime));
+        //ENGINE_LOG(std::to_string(deltaTime));
 
         if (Engine::Input::IsKeyPressed(GLFW_KEY_W)) {
             pos->position.y += deltaTime * vc_tag->cameraSpeed;
